53d9710963d4723f898d10a9516ed4b620b78490
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-07 17:58:07 +0100

    chore: create the file for the login page and assign it to a route in the App file

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Route } from "react-router-dom";
 import { Routes } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 import CreateAccount from "./pages/CreateAccount";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<CreateAccount />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
