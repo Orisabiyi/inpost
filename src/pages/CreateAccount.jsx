173c74f7f08ad8f34557a07f7f453efc68c558f5
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-07 17:53:03 +0100

    refactor: refactor the codebase for FormSideBar

--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -1,6 +1,6 @@
-import logo from "../assets/logo.svg";
 import linkedin from "../assets/linkedin.svg";
 import "../assets/fonts.css";
+import FormSidebar from "../components/FormSidebar";
 
 function CreateAccount() {
   return (
@@ -8,24 +8,7 @@ function CreateAccount() {
       className="flex items-center bg-gray-100 p-8 gap-40 2xl:container 2xl:mx-auto"
       style={{ fontFamily: "Filson Pro", fontWeight: "400" }}
     >
-      <section className="flex flex-col items-start justify-between flex-auto w-1/2 min-h-screen bg-blue-200 rounded-4 p-12">
-        <img src={logo} alt="inpost company logo" />
-
-        <div className="text-gray-600">
-          <p className="text-16 mb-4">Step 1/4</p>
-          <h1 className="flex flex-col items-start text-60 leading-none">
-            <span>Content</span>
-            <span>made for</span>
-            <span>you.</span>
-            <div className="flex items-center justify-center gap-4 mt-10">
-              <span className="bg-blue-600 w-36 h-6 rounded-full"></span>
-              <span className="bg-white w-6 h-6 rounded-full"></span>
-              <span className="bg-white w-6 h-6 rounded-full"></span>
-              <span className="bg-white w-6 h-6 rounded-full"></span>
-            </div>
-          </h1>
-        </div>
-      </section>
+      <FormSidebar />
 
       <section className="flex-auto w-1/2 flex flex-col items-start justify-center gap-10">
         <h3
