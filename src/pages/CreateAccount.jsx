132d578f038b4fd56d50143ef5d720120f94f312
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-07 14:34:22 +0100

    feat: created a form for registration

--- src/pages/CreateAccount.jsx
+++ src/pages/CreateAccount.jsx
@@ -2,7 +2,7 @@ import logo from "../assets/logo.svg";
 
 function CreateAccount() {
   return (
-    <main className="flex items-center bg-gray-100 p-8">
+    <main className="flex items-center bg-gray-100 p-8 gap-40">
       <section className="flex flex-col items-start justify-between flex-auto w-1/2 min-h-screen bg-blue-200 rounded-4 p-12">
         <img src={logo} alt="inpost logo" />
 
@@ -11,13 +11,49 @@ function CreateAccount() {
           <h1 className="flex flex-col items-start text-60 leading-none">
             <span>Content</span>
             <span>made for</span>
-            <span>you</span>
+            <span>you.</span>
           </h1>
         </div>
       </section>
 
-      <section className="flex-auto w-1/2">
-        <form method="post">u</form>
+      <section className="flex-auto w-1/2 flex flex-col items-start justify-center gap-10">
+        <h3 className="flex flex-col text-32 leading-none">
+          <span>Create a free account</span>
+          <span>to explore a consistent</span>
+          <span>Linkedin brand</span>
+        </h3>
+
+        <form
+          method="post"
+          className="flex flex-col items-stretch justify-center text-gray-600"
+        >
+          <label htmlFor="email" className="text-16">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Username@example.com"
+            className="border-2 w-[45rem] bg-gray-200 rounded-full text-16 py-4 px-6 mt-3 mb-6 outline-none"
+          />
+
+          <label htmlFor="password" className="text-16">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            placeholder="***************"
+            className="border-2 w-[45rem] bg-gray-200 rounded-full text-16 py-4 px-6 mt-3 mb-6 outline-none"
+          />
+
+          <button
+            type="submit"
+            className="bg-blue-400 text-white py-4 text-16 rounded-full mt-8"
+          >
+            Create an account
+          </button>
+        </form>
       </section>
     </main>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,17 @@ module.exports = {
     extend: {
       colors: {
         "gray-100": "#FDFDFD",
+        "gray-200": "#F3F3F3",
         "gray-600": "#2B2B2B",
         "blue-200": "#CDDFFB",
+        "blue-400": "#055FE9",
       },
       borderRadius: {
         4: "4rem",
       },
       fontSize: {
         16: "1.6rem",
+        32: "3.2rem",
         60: "6rem",
       },
     },
