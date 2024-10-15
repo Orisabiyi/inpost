923a73d1fb09927de5d6e4e0c3f3e832fb75276d
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-15 12:29:05 +0100

    chore: change the content and include the sidebar

--- src/pages/Connect.jsx
+++ src/pages/Connect.jsx
@@ -4,14 +4,21 @@ import Section from "../components/Section";
 function Connect() {
   return (
     <Main>
+      <FormSidebar>
+        <>
+          <span>Connect</span>
+          <span>your social</span>
+          <span>accounts</span>
+        </>
+      </FormSidebar>
+
       <Section>
         <h1
           className="flex flex-col text-32 leading-none"
           style={{ fontFamily: "Filson Pro", fontWeight: "500" }}
         >
-          <span>Connect</span>
-          <span>your social</span>
-          <span>accounts</span>
+          <span>Connect your social media</span>
+          <span>accounts to inpost</span>
         </h1>
       </Section>
     </Main>
