1988b5956443f2436e8c445ad505bc6c6374af6c
Orisabiyi <orisabiyidavid@gmail.com>
2024-10-18 17:21:26 +0100

    feat: setup the credit purchasing page

---
+++ src/pages/Credit.jsx
@@ -0,0 +1,18 @@
+import FormSidebar from "../components/FormSidebar";
+import Main from "../components/Main";
+
+function Credit() {
+  return (
+    <Main>
+      <FormSidebar>
+        <>
+          <span>Purchase</span>
+          <span>Creation</span>
+          <span>credit.</span>
+        </>
+      </FormSidebar>
+    </Main>
+  );
+}
+
+export default Credit;
